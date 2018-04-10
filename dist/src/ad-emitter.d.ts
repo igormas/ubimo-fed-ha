081fb146647467617429b3a1d017fbfcd0d7a38f
Igor Masliansk <igor.masliansky@gmail.com>
2018-04-10 17:55:16 +0300

    build

--- dist/src/ad-emitter.d.ts
+++ dist/src/ad-emitter.d.ts
@@ -1,4 +1,19 @@
-export declare class AdEmitter {
-    private _adEmitter;
-    hello(): void;
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/expand';
+import 'rxjs/add/operator/delay';
+export interface IAdEvent {
+    type: 'IMAGE' | 'VIDEO';
+    src: string;
+    name: string;
 }
+export declare class AdEmitter {
+    private _adEmitter$;
+    constructor();
+    private getRandomAdd();
+    private startEmissions();
+    readonly adEmitter$: Observable<IAdEvent>;
+    registerToAdEvents(cb: (adEvent: IAdEvent) => void): {
+        removeListener: () => void;
+    };
+}
+export declare const adDispatcher: AdEmitter;
